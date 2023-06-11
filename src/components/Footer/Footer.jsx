52529e58fe966575df6a70839bc2a8530829a9fb
dyenta <mochammad.dyenta@ui.ac.id>
2023-06-11 16:32:21 +0700

    footer home

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,140 +1,37 @@
-import React, {useEffect} from "react";
-import "./footer.css"
-
-import{Container, Row, Col, ListGroup, ListGroupItem} from 'reactstrap'
-import {Link, useLocation} from 'react-router-dom'
-import logo from "../../assets/images/logo.jpg"
-
-const menu = [
-    {
-        path: '/home',
-        display: 'Home'
-    },
-    {
-        path: '/about',
-        display: 'About'
-    },
-    {
-        path: '/tour',
-        display: 'Tours'
-    },
-]
-
-const company = [
-    {
-        path: '/help',
-        display: 'Help/FAQ'
-    },
-    {
-        path: '/gallery',
-        display: 'Gallery'
-    },
-    {
-        path: '/affilates',
-        display: 'Affilates'
-    },
-]
-
+import React from "react";
+import { FaTwitter, FaInstagram, FaFacebookSquare } from "react-icons/fa";
+import logo from "../../assets/images/logo1.png"
+import './footer.css'
 const Footer = () => {
-
-    const year = new Date().getFullYear()
-    const location = useLocation();
-
-    useEffect(() => {
-        window.scrollTo(0, 0); // Scroll to top when component mounts or location changes
-    }, [location]);
-
-    return (
-        <footer className="footer">
-            <Container>
-                <Row>
-                    <Col lg='3'>
-                        <div className="logo">
-                            <img src={logo} alt=""/>
-                            <p>Book your trip in a minute, get fully immerse in to the culture.</p>
-
-                            <div className="social_links d-flex align-items-center gap-4">
-                                <span>
-                                    <Link to='#'><i class="ri-youtube-line"></i></Link>
-                                </span>
-                                <span>
-                                    <Link to='#'><i class="ri-github-fill"></i></Link>
-                                </span>
-                                <span>
-                                    <Link to='#'><i class="ri-facebook-circle-line"></i></Link>
-                                </span>
-                                <span>
-                                    <Link to='#'><i class="ri-instagram-line"></i></Link>
-                                </span>
-                            </div>
-                        </div>
-                    </Col>
-
-                    <Col lg='3'>
-                        <h5 className="footer_link-title">Menu</h5>
-
-                        <ListGroup className="footer_menu">
-                            {
-                                menu.map((item, index) => (
-                                    <ListGroupItem key={index} className="ps-0 border-0">
-                                        <Link to={item.path}>{item.display}</Link>
-                                    </ListGroupItem>
-                                ))
-                            }
-                        </ListGroup>
-                    </Col>
-                    <Col lg='3'>
-                        <h5 className="footer_link-title">Company</h5>
-
-                        <ListGroup className="footer_company">
-                            {
-                                company.map((item, index) => (
-                                    <ListGroupItem key={index} className="ps-0 border-0">
-                                        <Link to={item.path}>{item.display}</Link>
-                                    </ListGroupItem>
-                                ))
-                            }
-                        </ListGroup>
-                    </Col>
-                    <Col lg='3'>
-                        <h5 className="footer_link-title">Contact</h5>
-
-                        <ListGroup className="footer_quick-links">
-                            <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-3">
-                                <h6 className="mb-0 d-flex align-items-center gap-2"> 
-                                    <span><i class="ri-map-pin-line"></i></span>
-                                    Address: 
-                                </h6>
-
-                                <p className="mb-0">DTE FTUI 2021, Depok, Jawa Barat</p>
-                            </ListGroupItem>
-                            <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-3">
-                                <h6 className="mb-0 d-flex align-items-center gap-2"> 
-                                    <span><i class="ri-mail-line"></i></span>
-                                    E-mail: 
-                                </h6>
-
-                                <p className="mb-0">amrita.deviayu@ui.ac.id</p>
-                            </ListGroupItem>
-                            <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-3">
-                                <h6 className="mb-0 d-flex align-items-center gap-2"> 
-                                    <span><i class="ri-phone-fill"></i></span>
-                                    Phone: 
-                                </h6>
-
-                                <p className="mb-0">021-3456-7890</p>
-                            </ListGroupItem>
-                        </ListGroup>
-                    </Col>
-
-                    <Col lg='12' className="text-center pt-5">
-                        <p className="copyright">Copyright {year}, design and develop by Amrita Deviayu Tunjungbiru.
-                        All rights reserved.</p>
-                    </Col>
-                </Row>
-            </Container>
-        </footer>
-    )
+  return (
+    <footer className="footer-wrapper">
+      <div className="logo-wrapper">
+        <div>
+          <img src={logo} alt="" />
+        </div>
+        <div>
+          <div className="menu-wrapper">
+            Menu
+            <a>Home</a>
+            <a>Term and Condition</a>
+          </div>
+          <div className="menu-wrapper">
+            Contact
+            <span>Email: gofly@gmail.com</span>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div className="sosmed-wrapper">
+        <div>
+          <FaTwitter />
+          <FaInstagram />
+          <FaFacebookSquare />
+        </div>
+        <p>© 2023 GoFly incorporated</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
